103fee0ec3c42d91d9b6a2b1f7a02dbe62e8cae0
Simon Cross <hodgestar@gmail.com>
2014-11-29 18:01:42 +0200

    Clean-up manage.py.

--- manage.py
+++ manage.py
@@ -3,10 +3,6 @@ import os
 import sys
 
 if __name__ == "__main__":
-    conf = os.path.dirname(__file__)
-    wafer = os.path.join(conf, '..', 'wafer')
-    sys.path.append(wafer)
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
